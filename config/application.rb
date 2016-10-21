aff66ca81be0128cd7dc2493c2f67a7d888c2990
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-21 12:05:38 +0200

    update de application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Lala
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("public", "assets", "stylesheets")
+    config.assets.paths << Rails.root.join("public", "assets", "javascripts")  
+    
+    config.filter_parameters << :password
+    
+    config.generators do |g|
+      g.template_engine :slim
+    end
+
   end
 end
