fd2a38dbfe3cb62a3c973549c883e58b7c124a10
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-26 16:32:03 +0200

    lala

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,17 +16,17 @@ equipes = ["Paris", "Marseille", "Bordeaux", "Rennes"]
 
 championnats.each do | championnat |
     [0..5].each do |indice|
-        home_predic = [0..50].sample
-        away_predic = [0..50].sample
+        home_predic = rand(0..50)
+        away_predic = rand(0..50)
         Match.create(
             matchday: indice,
             home_team: equipes.sample,
             home_prevision: home_predic,
-            home_score: [0..3].sample,
-            draw_prevision: 100 - away_predic - home_predic,
+            home_score: rand(0..3),
+            draw_prevision: 100.0 - away_predic - home_predic,
             away_team: equipes.sample,
             away_prevision: away_predic,
-            away_score: [0..3].sample,
+            away_score: rand(0..3),
             championnat: championnat
         )
     end
