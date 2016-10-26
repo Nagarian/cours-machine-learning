a16176ca911135348d589e40d80cecd71b1d99fa
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-26 06:38:55 +0000

    ajout du controller match

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :matches
   resources :championnats
   get 'admin/index'
   get '/admin', to: 'admin#index'
   get 'welcome/index'
   resources :championnats
+  resources :matches
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
