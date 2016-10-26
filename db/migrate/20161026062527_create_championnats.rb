ed46419162aaa57d51fae0dbbaaa98d393d3f697
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-26 08:36:23 +0200

    update db

--- db/migrate/20161026062527_create_championnats.rb
+++ db/migrate/20161026062527_create_championnats.rb
@@ -1,8 +1,8 @@
 class CreateChampionnats < ActiveRecord::Migration[5.0]
   def change
     create_table :championnats do |t|
-
-      t.timestamps
+      t.string :name
+      t.timestamps null: false
     end
   end
 end
