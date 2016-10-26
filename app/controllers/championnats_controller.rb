ef3cc400eb007ece5bf44c94c9c210ea73ed4830
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-26 09:19:03 +0200

    CRUD championnat

--- app/controllers/championnats_controller.rb
+++ app/controllers/championnats_controller.rb
@@ -1,2 +1,48 @@
 class ChampionnatsController < ApplicationController
+ 
+    def index
+        @championnats = Championnat.all
+    end
+    
+    def show
+        @championnat = Championnat.find(params[:id])
+    end 
+
+    def new
+        @championnat = Championnat.new
+    end
+
+    def create
+        @championnat = Championnat.new(championnat_params)
+        if @championnat.save
+            redirect_to championnats_path
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+        @championnat = Championnat.find(params[:id])
+    end
+    
+    def update
+        @championnat = Championnat.find(params[:id])
+        if @championnat.update(championnat_params)
+            redirect_to @championnat
+        else
+            render 'edit'
+        end
+    end 
+
+    def destroy
+        @championnat = Championnat.find(params[:id])
+        @championnat.destroy
+        redirect_to championnats_path
+    end
+
+    private
+    def championnat_params
+        params.require(:championnat).permit(:name)
+    end 
+
 end
