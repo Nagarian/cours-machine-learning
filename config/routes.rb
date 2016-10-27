5f1ecbd76bda610791273db6a3e29699c5e16957
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-27 09:07:25 +0200

    github

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :championnats
   get 'admin/index'
   get '/admin', to: 'admin#index'
+  get '/admin/github', to: 'admin#github'
   get 'welcome/index'
   resources :championnats
   resources :matches
