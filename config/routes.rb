bb34b7d1e1324f6e166ade60822cdda5f588315e
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-27 16:09:30 +0200

    lala

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :matches
-  resources :championnats
-  
+
   root 'welcome#index'
   
-  get '/welcome/:id', to: 'welcome#index'
   get '/admin', to: 'admin#index'
   post '/admin/github', to: 'admin#github'
+
   resources :championnats
   resources :matches
+
+  get '/:id', to: 'welcome#index', as: "lala"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
