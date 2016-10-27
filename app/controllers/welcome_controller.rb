f0e8da9ec23c6d291a8c2e83e5ca2fad49eaaa91
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-27 16:13:50 +0200

    lala

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @championnats = Championnat.all
-    @championnat = Championnat.find_by(name: (params[:id] || "Ligue 1"))
+    
+    @championnat = (Championnat.find(params[:id]) || Championnat.find_by(name: "Ligue 1"))
+
     @matches = Match.where(championnat: @championnat).order(:matchday).paginate(:page => params[:page], :per_page => 3)
   end
 end
