35f7cefda8686c2e6ba2d3fd5ece5dddeb78eb0b
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-27 14:45:26 +0200

    lala

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @championnat = Championnat.find_by(name: "Ligue 1")
+    @matches = Match.where(championnat: @championnat).paginate(:page => params[:page], :per_page => 3)
   end
 end
