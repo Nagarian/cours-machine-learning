69f654d26f928e5219939492f2f7f28fada7c552
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-27 09:51:22 +0200

    lala

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,12 @@ class AdminController < ApplicationController
   
   def index
     @matches = Match.all
+    @datas = []
+    Championnat.each do |championnat|
+      championnat.matches.each do |match|
+        @datas.push([championnat.name, match.home_score > match.away_score ? match.home_team : match.home_score == match.away_score ? "draw" : match.away_team])
+      end
+    end
   end
 
   def github
