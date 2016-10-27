dc7b162ad6f34a8db1e782f4a7f9464e16d20843
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-10-27 16:10:53 +0200

    lala

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :championnats
   resources :matches
 
-  get '/:id', to: 'welcome#index', as: "lala"
+  get '/:id', to: 'welcome#index', as: "select_champ"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
