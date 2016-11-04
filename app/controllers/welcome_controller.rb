0100791f8784ce1335935a6ddefc65792998fbe0
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-11-04 16:47:46 +0100

    ajout du taux de réussite de la prédiction

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,9 @@ class WelcomeController < ApplicationController
     @championnat = Championnat.find((params[:id] || 1))
 
     @matches = Match.where(championnat: @championnat).where(championnat_year: "2016_2017").order(matchday: :desc).paginate(:page => params[:page], :per_page => 10)
+
+    if @matches.all? { |match|  match.home_prevision != nil && match.home_score != nil }
+      @good_prediction = @matches.count { |match| (match.draw_prevision > match.home_prevision && match.draw_prevision > match.away_prevision && match.home_score == match.away_score) || (match.home_prevision > match.away_prevision && match.home_score > match.away_score) || (match.home_prevision < match.away_prevision && match.home_score < match.away_score) }
+    end
   end
 end
