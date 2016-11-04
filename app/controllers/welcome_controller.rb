65089d54baf36054572fcc8184caa86eaf8b0e96
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-11-04 12:21:31 +0100

    lala

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,6 @@ class WelcomeController < ApplicationController
     
     @championnat = Championnat.find((params[:id] || 1))
 
-    @matches = Match.where(championnat: @championnat).order(:matchday).paginate(:page => params[:page], :per_page => 10)
+    @matches = Match.where(championnat: @championnat).where(championnat_year: "2016-2017").order(:matchday).paginate(:page => params[:page], :per_page => 10)
   end
 end
