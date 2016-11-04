1e8fe6a0dbc6acb2c683f9eebb2bd403f8de5b78
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-11-04 11:05:18 +0100

    ajout du champs championnat_year

--- db/migrate/20161026063703_create_matches.rb
+++ db/migrate/20161026063703_create_matches.rb
@@ -1,14 +1,15 @@
 class CreateMatches < ActiveRecord::Migration[5.0]
   def change
     create_table :matches do |t| 
-      t.integer :matchday       # journée de championnat
-      t.string :home_team       # équipe à domicile
-      t.float :home_prevision   # prévision score domicile
-      t.integer :home_score     # score réel domicile
-      t.float :draw_prevision   # prévision match nul
-      t.string :away_team       # équipe à l'extérieur
-      t.float :away_prevision   # prévision score extérieur
-      t.integer :away_score     # score réel extérieur
+      t.integer :matchday         # journée de championnat
+      t.integer :championnat_year # année de championnat
+      t.string :home_team         # équipe à domicile
+      t.float :home_prevision     # prévision score domicile
+      t.integer :home_score       # score réel domicile
+      t.float :draw_prevision     # prévision match nul
+      t.string :away_team         # équipe à l'extérieur
+      t.float :away_prevision     # prévision score extérieur
+      t.integer :away_score       # score réel extérieur
       t.timestamps null: false
       t.belongs_to :championnat, index: true  # championnat_id
       t.timestamps null: false 
