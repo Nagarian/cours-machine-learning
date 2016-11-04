56c4fe942341666530712ff70e856d24c3062643
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-11-04 12:10:41 +0100

    lala

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,17 +56,33 @@ files.each do |file|
   CSV.foreach("public/data/" + file + ".csv", headers: true) do |row|
     row_pre_insert = row.to_hash.select { |k, v| fields_to_insert.include?(k) }
     
-    row_to_insert = {
-        "matchday" => matchday,
-        "championnat_year" => file,
-        "home_team" => row_pre_insert["HomeTeam"],
-        "home_score" => row_pre_insert["FTHG"].to_i,
-        "away_team" => row_pre_insert["AwayTeam"],
-        "away_score" => row_pre_insert["FTAG"].to_i,
-        "championnat_id" => ligue1.id
-    }
+    # row_to_insert = [
+    #     "matchday" => matchday,
+    #     "championnat_year" => file,
+    #     "home_team" => row_pre_insert["HomeTeam"],
+    #     "home_prevision" => 0,
+    #     "home_score" => row_pre_insert["FTHG"].to_i,
+    #     "draw_prevision" => 0,
+    #     "away_team" => row_pre_insert["AwayTeam"],
+    #     "away_score" => row_pre_insert["FTAG"].to_i,
+    #     "away_prevision" => 0,
+    #     "championnat_id" => ligue1.id
+    # ]
 
-    rows_to_insert << row_to_insert
+    Match.create(
+        matchday: matchday,
+        championnat_year: file,
+        home_team: row_pre_insert["HomeTeam"],
+        home_prevision: 0,
+        home_score: row_pre_insert["FTHG"].to_i,
+        draw_prevision: 0,
+        away_team: row_pre_insert["AwayTeam"],
+        away_score: row_pre_insert["FTAG"].to_i,
+        away_prevision: 0,
+        championnat_id: ligue1.id
+    )
+
+    # rows_to_insert << row_to_insert
 
     if matchday_compteur < 10
         matchday_compteur += 1
@@ -77,4 +93,4 @@ files.each do |file|
   end
 end
 
-Match.import(rows_to_insert)
+# Match.import(rows_to_insert)
